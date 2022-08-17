892f407d6df34c2af5a58835c61da8b6f0d05446
Lyman Li <limianfeng12@gmail.com>
2022-08-18 00:48:37 +0800

    优化静态分屏滤镜

---
+++ SimpleCam/Filter/Split/SCGPUImageStaticSplitFilter.h
@@ -0,0 +1,13 @@
+//
+//  SCGPUImageStaticSplitFilter.h
+//  SimpleCam
+//
+//  Created by 李棉烽 on 2022/8/18.
+//  Copyright © 2022 Lyman Li. All rights reserved.
+//
+
+#import "SCGPUImageCustomFilter.h"
+
+@interface SCGPUImageStaticSplitFilter : SCGPUImageCustomFilter
+
+@end
---
+++ SimpleCam/Filter/Split/SCGPUImageStaticSplitFilter.m
@@ -0,0 +1,52 @@
+//
+//  SCGPUImageStaticSplitFilter.m
+//  SimpleCam
+//
+//  Created by 李棉烽 on 2022/8/18.
+//  Copyright © 2022 Lyman Li. All rights reserved.
+//
+
+#import "SCGPUImageStaticSplitFilter.h"
+
+NSString * const kSCGPUImageStaticSplitFilterShaderString = SHADER_STRING
+(
+ precision highp float;
+
+ uniform sampler2D inputImageTexture;
+ varying vec2 textureCoordinate;
+
+ uniform float horizontal;
+ uniform float vertical;
+
+ void main (void) {
+    // 限制最小分割
+    float horizontalCount = max(horizontal, 1.0);
+    float verticalCount = max(vertical, 1.0);
+    // 长宽比
+    float ratio = verticalCount / horizontalCount;
+
+    vec2 originSize = vec2(1.0, 1.0);
+    vec2 newSize = originSize;
+
+    if (ratio > 1.0) { // 竖直方向的分割数比较多，说明竖直方向需要裁剪
+        newSize.y = 1.0 / ratio;
+    } else {  // 水平方向的分割数比较多，说明水平方向需要裁剪
+        newSize.x = ratio;
+    }
+
+    vec2 offset = (originSize - newSize) / 2.0;
+    vec2 position = offset + mod(textureCoordinate * min(horizontalCount, verticalCount), newSize);
+
+    gl_FragColor = texture2D(inputImageTexture, position);
+ }
+
+);
+
+@implementation SCGPUImageStaticSplitFilter
+
+- (instancetype)init {
+    self = [super initWithVertexShaderFromString:nil fragmentShaderFromString:kSCGPUImageStaticSplitFilterShaderString];
+    return self;
+}
+
+@end
